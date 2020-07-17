feb4a12675c097b812ff7990d561a78a986aac51
rocksnow1942 <rocksnow1942@gmail.com>
2020-07-16 21:48:43 -0700

    ON: Thu Jul 16 21:48:42 2020, FROM: mini

--- views/menuView.py
+++ views/menuView.py
@@ -184,7 +184,7 @@ class PicoMethod(tk.Toplevel):
     """
     defaultCovid = {'channel':'C1','dtype':'covid-trace','show':True,'showPeak':True,'yMin':0,'yMax':0,
     'Auto E Range':False,'E Begin':-0.6,'E End':0,'E Step':0.005,'CurrentRange Min': '100 uA','CurrentRange Max':'100 uA',
-    'E Amp':0.05, 'Frequency':100,'Interval':20,'Duration(s)':2400,'Total Scans':960,'Wait time':0.2}
+    'E Amp':0.05, 'Frequency':100,'Interval':20,'Duration(s)':3600,'Total Scans':960,'Wait time':0.2}
     dummy = {'channel':'C1','dtype':'dummy-type','show':False,'dummy':0}
     defaultCovidScript = {'channel':'C1','dtype':'covid-trace-manualScript','show':True,'showPeak':True,
     'Interval':15,'Duration(s)':2400,'Total Scans':960,
