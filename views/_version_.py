90f9c178658b62dd2cc2006343a68a268f31d764
rocksnow1942 <rocksnow1942@gmail.com>
2020-10-05 12:28:49 -0700

    a

--- views/_version_.py
+++ views/_version_.py
@@ -1,7 +1,7 @@
-__version__="1.0.7"
+__version__="1.0.8"
 __updateNote__=f"""Version {__version__}
 Updates:
-
+10/05/20: add git pull to update, read data from reader Downloads.
 09/17/20: add reverse scan invert Amp to monitor.
 09/11/20: added data viewer app function.
 08/26/20: updated manual script method to collect all data packets.
