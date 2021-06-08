08ac422d459660314cc9aae85f00afe0d3ed3f2f
rocksnow1942 <rocksnow1942@gmail.com>
2021-06-08 13:33:54 -0700

    revise plot ct

--- plotCT.py
+++ plotCT.py
@@ -18,7 +18,7 @@ from itertools import combinations
 #### ╚═╝     ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚══════╝ ####
 #### Change this manually if running code in terminal.                      ####
 ################################################################################
-picklefile = r"C:\Users\hui\Desktop\non dtt buffer.picklez"
+picklefile = r"C:\Users\hui\RnD\Projects\LAMP-Covid Sensor\Data Export\20210607\20210607 NTC vs PTC.picklez"
 
 
 if __name__ == '__main__':
@@ -32,9 +32,6 @@ dataSource.load_picklefiles(pickleFiles)
 
 X, y, names,devices = removeDuplicates(*dataSource.exportXy())
 
-dataSource.rawView['data'][0].keys()
-
-dataSource.rawView['data'][0]['_channel']
 
 print('Total curve count is : '+str(len(X)))
 print("Total Positive Data: "+str(sum(y)))
@@ -52,6 +49,7 @@ smoothT = Pipeline([
     ('truncate', Truncate(cutoffStart=cutoffStart, cutoffEnd=cutoffEnd, n=90)),
     ('remove time', RemoveTime()),
 ])
+smoothed_X = smoothT.transform(X)
 
 deriT = Pipeline([
     ('smooth', Smoother(stddev=2, windowlength=11, window='hanning')),
@@ -60,21 +58,31 @@ deriT = Pipeline([
     ('Derivitive', Derivitive(window=31, deg=3)),
     # ('remove time',RemoveTime()),
 ])
+deri_X = deriT.transform(X)
 
-
-peaksT = Pipeline([
+tCtT = Pipeline([
     ('smooth', Smoother(stddev=2, windowlength=11, window='hanning')),
     ('normalize', Normalize(mode='mean', normalizeRange=(normStart, normEnd))),
     ('truncate', Truncate(cutoffStart=cutoffStart, cutoffEnd=cutoffEnd, n=90)),
     ('Derivitive', Derivitive(window=31, deg=3)),
-    ('peak', FindPeak())
+    ('peak', FindPeak()),
+    ('thresholdCt',ThresholdCt())
     # ('remove time',RemoveTime()),
 ])
+tCt_X = tCtT.transform(X)
 
 
-smoothed_X = smoothT.transform(X)
-deri_X = deriT.transform(X)
-peaks_X = peaksT.transform(X)
+tCtPredictT = Pipeline([
+    ('smooth', Smoother(stddev=2, windowlength=11, window='hanning')),
+    ('normalize', Normalize(mode='mean', normalizeRange=(normStart, normEnd))),
+    ('truncate', Truncate(cutoffStart=cutoffStart, cutoffEnd=cutoffEnd, n=90)),
+    ('Derivitive', Derivitive(window=31, deg=3)),
+    ('peak', FindPeak()),
+    ('thresholdCt',ThresholdCt()),
+    ('predictor',CtPredictor(ct=23,prominence=0.2,sd=0.131))
+])
+pred_X = tCtPredictT.transform(X)
+
 
 
 #############################################################################
@@ -88,7 +96,7 @@ peaks_X = peaksT.transform(X)
 # ██║     ███████╗╚██████╔╝   ██║   ██║     ██║  ██║██║  ██║██║  ██║        #
 # ╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝        #
 # This set how many columns in the figure, set to 0 automatically determine.#
-col = 0
+col = 4
 # ymin and ymax is the min and max of y axis
 ymin = 0.3
 ymax = 1.3
@@ -102,30 +110,33 @@ print(f'Generating curve plots in a {row} x {col} Grid')
 fig, axes = plt.subplots(row, col, figsize=(col*4, row*3))
 axes = [i for j in axes for i in j]
 
-for i, j in enumerate(y):
+for i,j in enumerate(y):
     ax = axes[i]
+    ax.set_ylim([0.4,1.3])
+    
     smoothed_c = smoothed_X[i]
-    t, deri, _ = deri_X[i]
-    left_ips, peak_prominence, peak_width, *sd = peaks_X[i]
-    curvePeakRange = findTimeVal(t, smoothed_c, left_ips, peak_width)
-    xvals = np.linspace(t[0], t[-1], len(deri))
-    ax.plot(xvals, smoothed_c, color='red' if y[i] else 'green')
-    ax.plot(np.linspace(left_ips, left_ips+peak_width, len(curvePeakRange)),
-            curvePeakRange, linewidth=4, alpha=0.75, color='cyan')
-    # set plot y axis limits
-    ax.set_ylim([ymin, ymax])
-
-    ax.plot(xvals, (deri - np.min(deri)) / (np.max(deri) - np.min(deri)) *
-            (np.max(smoothed_c)-np.min(smoothed_c)) + np.min(smoothed_c), '--', alpha=0.8)
-
-    p_n = 'Positive' if y[i] else 'Negative'
-    ax.set_title(f'Ct:{left_ips:.1f} Pm:{peak_prominence:.2f} M:{p_n} {devices[i]}',
-                 fontdict={'color': 'red' if y[i] else 'green'})
+    t,deri,_ =  deri_X[i]
+    left_ips,peak_prominence,peak_width, *sd= tCt_X[i]    
+    # find the threshold Ct    
+    thresholdline = np.poly1d(tCt_X[i][-3:-1])
+    thresholdCt = tCt_X[i][-1]
+    curvePeakRange = findTimeVal(t,smoothed_c,left_ips,peak_width)
+    xvals = np.linspace(t[0],t[-1],len(deri))
+    # plot smoothed current
+    ax.plot(xvals,smoothed_c,color='red' if y[i] else 'green')
+    # plot the signal drop part
+    ax.plot(np.linspace(left_ips,left_ips+peak_width,len(curvePeakRange)) ,curvePeakRange,linewidth=4,alpha=0.75 )
+    # plot plot the derivative peaks
+    ax.plot(xvals,(deri - np.min(deri) ) / (np.max(deri) -np.min(deri) ) * (np.max(smoothed_c)-np.min(smoothed_c)) + np.min(smoothed_c),'--',alpha=0.8)
+    # ax.plot(xvals,fitres(xvals),'b-.')
+    ax.plot(xvals,thresholdline(xvals),'b-.',alpha=0.7)
+    ax.plot([thresholdCt,thresholdCt],[0,2],'k-')
+    p_n = '+' if pred_X[i][0] else '-'
+    ax.set_title(f'Ct:{left_ips:.1f} tCt:{thresholdCt:.1f} Pm:{peak_prominence:.2f} SD5:{sd[2]:.2f} P:{p_n}',
+    fontdict={'color':'red' if y[i]!=pred_X[i][0] else 'green','fontsize':10})
     ax.set_xlabel('\n'.join(textwrap.wrap(
-        names[i].strip(), width=45)), fontdict={'fontsize': 10})
-
+        names[i].strip(), width=45)), fontdict={'fontsize': 10})        
 plt.tight_layout()
-
 
 # save to figure
 fig.savefig(picklefile+'.'+format,dpi=300)
@@ -133,7 +144,7 @@ print(f"Curve plot is saved to {picklefile+'.'+format}.")
 
 
 features = ['Ct', 'Prominence', 'Peak_Width', 'SD_Peak_Width',
-            'SD_3min', 'SD_5min', 'SD_10min', 'SD_15min', 'SD_End']
+            'SD_3min', 'SD_5min', 'SD_10min', 'SD_15min', 'SD_End','fit_a','fit_b','thresholdCt']
 
 # write result to csv file
 with open(f'{picklefile}.csv', 'w', newline='') as f:
@@ -141,19 +152,19 @@ with open(f'{picklefile}.csv', 'w', newline='') as f:
     writer.writerow(['Name', 'P/N','Device']+features)
     for i, j in enumerate(y):
         name = names[i].strip()
-        _ = list(peaks_X[i])
+        _ = list(tCt_X[i])
         writer.writerow([name, 'Positive' if j else 'Negative',devices[i]] + _)
 print(f"Write Ct and Prominence data to {picklefile+'.csv'}.")
 
 
 # plot scatter plot of different features
-fig, axes = plt.subplots(6, 6, figsize=(22, 20))
-axes = [i for j in axes for i in j]
-for (i, j), ax in zip(combinations(range(9), 2), axes):
+fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+# axes = [i for j in axes for i in j]
+for (i, j), ax in zip(combinations([1,5,-1], 2), axes):
     il = features[i]
     jl = features[j]
-    ax.plot(peaks_X[y == 0, i], peaks_X[y == 0, j], 'gx', label='Negative')
-    ax.plot(peaks_X[y == 1, i], peaks_X[y == 1, j], 'r+', label='Positive')
+    ax.plot(tCt_X[y == 0, i], tCt_X[y == 0, j], 'gx', label='Negative')
+    ax.plot(tCt_X[y == 1, i], tCt_X[y == 1, j], 'r+', label='Positive')
     ax.set_title(f'{il} vs {jl}')
     ax.set_xlabel(il)
     ax.set_ylabel(jl)
