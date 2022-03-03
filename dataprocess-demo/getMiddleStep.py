46321a236299dfb6bf4703bf56e05bf37e91d3e0
rocksnow1942 <rocksnow1942@gmail.com>
2022-03-02 21:23:02 -0800

    update

--- dataprocess-demo/getMiddleStep.py
+++ dataprocess-demo/getMiddleStep.py
@@ -7,11 +7,20 @@ from util import *
 import numpy as np
 from util import hCtTPredictT,convert_list_to_X
 from pathlib import Path
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.family'] = ['Heiti TC']
 
-def output(file):
+
+def output(file,container=[]):
     def wrap(msg):
         with open(file, 'a') as f:
-            f.write(msg + '\n')
+            if isinstance(msg, str):
+                container.append([msg])
+                f.write(msg + '\n')
+            else:
+                container[-1].append(msg)                
+                f.write(json.dumps(msg) + '\n')
     return wrap
 
 def main(file):
@@ -22,7 +31,9 @@ def main(file):
     outdir = f.parent / 'calcStep'
     outdir.mkdir(exist_ok=True)
 
-    addToFile = output(outdir/(f.name + '.txt'))
+    container = []
+    addToFile = output(outdir/(f.name + '.txt'),container)
+    imgFile = outdir / (f.name+'.png')
 
 
     fits = []
@@ -38,7 +49,7 @@ def main(file):
     data = [[t,c]]
     
     addToFile("原始数据图:")
-    addToFile(json.dumps([list(np.linspace(0,30,90)),list(c)]))
+    addToFile([list(np.linspace(0,30,90)),list(c)])
 
     # the convert_list_to_X is just to transform the data to the shape that works with the scipy pipeline.
     X = convert_list_to_X(data)
@@ -50,9 +61,7 @@ def main(file):
     ])
     smoothed_X = smoothT.transform(X)
     addToFile("图1:")
-    addToFile(json.dumps([list(np.linspace(0,30,90)),list(smoothed_X[0][1])]))
-
-
+    addToFile([list(smoothed_X[0][0]),list(smoothed_X[0][1])])
 
     # fig 2
     smoothT = Pipeline([
@@ -61,7 +70,7 @@ def main(file):
     ])
     smoothed_X = smoothT.transform(X)
     addToFile("图2:")
-    addToFile(json.dumps([list(np.linspace(0,30,90)),list(smoothed_X[0][1])]))
+    addToFile([list(smoothed_X[0][0]),list(smoothed_X[0][1])])
 
 
     # fig 3
@@ -72,7 +81,9 @@ def main(file):
     ])
     smoothed_X = smoothT.transform(X)
     addToFile("图3:")
-    addToFile(json.dumps([list(smoothed_X[0][0]),list(smoothed_X[0][1])]))
+    ts,te = smoothed_X[0][0][0],smoothed_X[0][0][-1]
+    timeAfterTruncate = list(np.linspace(ts,te,len(smoothed_X[0][1])))
+    addToFile([timeAfterTruncate,list(smoothed_X[0][1])])
 
 
     # fig 4
@@ -85,7 +96,7 @@ def main(file):
     ])
     smoothed_X = smoothT.transform(X)
     addToFile("图4:")
-    addToFile(json.dumps([list(smoothed_X[0][0]),list(-smoothed_X[0][1])]))
+    addToFile([timeAfterTruncate,list(-smoothed_X[0][1])])
 
 
     # fig6
@@ -97,7 +108,7 @@ def main(file):
     ])
     smoothed_X = smoothT.transform(X)
     addToFile("图6:")
-    addToFile(json.dumps([list(smoothed_X[0][0]),list(smoothed_X[0][1])]))
+    addToFile([timeAfterTruncate,list(smoothed_X[0][1])])
 
 
 
@@ -111,21 +122,50 @@ def main(file):
         ('peak', FindPeak()),
         ('logCt',HyperCt(offset=0.05)),    
     ])
+   
     hCtT_X = hCtT.transform(X)
+
+    # prediction 
+    hCtTPredictT = Pipeline([
+    ('smooth', Smoother(stddev=2, windowlength=11, window='hanning')),
+    ('normalize', Normalize(mode='mean', normalizeRange=(normStart, normEnd))),
+    ('truncate', Truncate(cutoffStart=cutoffStart, cutoffEnd=cutoffEnd, n=90)),
+    ('Derivitive', Derivitive(window=31, deg=3)),
+    ('peak', FindPeak()),
+    ('logCt',HyperCt()),
+    ('predictor',SdPrPredictor(prominence=0.2,sd=0.106382))
+])
+    hCtpred_X = hCtTPredictT.transform(X)
+
     hyperline = HyperCt.hyperF(None,hCtT_X[0][-4:-1])
     xvals = smoothed_X[0][0]
 
 
-    addToFile("图8:")
     addToFile(f"双曲线拟合参数: p = {list(hCtT_X[0][-4:-1])} ")
-    addToFile(json.dumps([list(xvals),list(hyperline(xvals))]))
+    addToFile("图8:")    
+    addToFile([list(xvals),list(hyperline(xvals))])
 
     left_ips, prominance,pw,sd0,sd3,sd5,*_,Ct = hCtT_X[0]
     addToFile("最终结果:")
     addToFile(f"Pr = {prominance}, Sd = {sd5}, left_ips={left_ips}, Ct = {Ct}")
+    addToFile(f"Call = {hCtpred_X[0][-1]}")
+
+
+    # plot 
+    toPlot = [i for i in container if len(i)>1]
+    fig, axes = plt.subplots(len(toPlot), 1, figsize=(5, len(toPlot)*3))
+
+    for i,(name,j) in enumerate(toPlot):
+        ax = axes[i]
+        ax.plot(j[0],j[1])
+        ax.set_title(name)
+    plt.tight_layout()
+
+    # save to figure
+    fig.savefig(imgFile,dpi=300)
 
 
 import glob
-files = glob.glob("/Users/hui/Work/Covid_Reader/TWXY/20220204dataSentToTWXY/rawData/*.json")
+files = glob.glob("/Users/hui/Desktop/data/*.json")
 for file in files:
     main(file)
--- dataprocess-demo/twxyValidate.py
+++ dataprocess-demo/twxyValidate.py
@@ -1,26 +1,36 @@
-
 import json
 
-raw = r"C:\Users\hui\Desktop\TWXY_Raw2.txt"
 
+files = """/Users/hui/Desktop/20220302-TWXY-PCBA-data/20220302-CH0=negative, CH3=postive-Raw1.txt
+/Users/hui/Desktop/20220302-TWXY-PCBA-data/20220302-CH0=negative, CH3=postive-Raw2.txt
+/Users/hui/Desktop/20220302-TWXY-PCBA-data/20220302-CH0=negative, CH3=postive-Raw3.txt
+/Users/hui/Desktop/20220302-TWXY-PCBA-data/20220302-CH0=negative, CH3=postive-Raw4.txt
+/Users/hui/Desktop/20220302-TWXY-PCBA-data/20220302-CH0CH3-both-postive-Raw1.txt
+/Users/hui/Desktop/20220302-TWXY-PCBA-data/20220302-CH0CH3-both-postive-Raw2.txt""".split('\n')
 
-data = open(raw,'rt',encoding='utf-8').read().split('\n')
+for f in files:
+    data = open(f,'rt',encoding='utf-8').read().split('\n')
 
-raw = {0:[],3:[]}
+    raw = {0:[],3:[]}
 
-ch = 0
-for d in data:
-    if len(d) > 120:
-        i0=d.index('{')
-        i1=d.index('}')
-        j = json.loads(d[i0:i1+1])
-        raw[ch].append([[-0.5,0.1],j['c']])
-        ch = 0 if ch else 3
-        
+    ch = 0
+    for i,d in enumerate(data):
+        if '{' in d:                    
+            i0=d.index('{')
+            if '}' in d:
+                i1=d.index('}')
+                jd = d[i0:i1+1]
+            else:
+                i1=data[i+1].index('}')
+                jd = d[i0:] + data[i+1][i0:i1+1]                
+            j = json.loads(jd)            
+            jc = j.get('c',[])
+            if (len(jc))> 50:
+                raw[ch].append([[-0.5,0.1],j['c']])
+                ch = 0 if ch else 3
+            
 
-for ch in [0,3]:
-    with open(f'twx2{ch}.json','wt') as f:
-        json.dump(raw[ch],f)
-
-
+    for ch in [0,3]:
+        with open(f'{f}_ch{ch}.json','wt') as fobj:
+            json.dump(raw[ch],fobj)
 
